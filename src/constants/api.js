46778ed34641f5e9ecd2c06629668a20379180e6
MJ Phillip <32121892+MJPhillip@users.noreply.github.com>
2021-09-21 15:57:32 +0100

    Update api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,1 @@
-// The following can be used to get the list of books, but won't work to get specific details https://gist.githubusercontent.com/jaaneh/ab76310b62eccdf8c0657f92319c1568/raw/5b811320a276fa20e522bcf08eeb95b49143e9e4/books.json
-export const API = "https://t9jt3myad3.execute-api.eu-west-2.amazonaws.com/api/books";
+export const API = "https://noroff.herokuapp.com/v1/books";
