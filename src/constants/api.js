6239fc9d5f650f319186ff5f328384dfefcf7fb3
MJ Phillip <32121892+MJPhillip@users.noreply.github.com>
2021-09-21 09:41:29 +0100

    Changed API url to working version of JSON

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,1 +1,1 @@
-export const API = "https://t9jt3myad3.execute-api.eu-west-2.amazonaws.com/api/books";
+export const API = "https://gist.githubusercontent.com/jaaneh/ab76310b62eccdf8c0657f92319c1568/raw/5b811320a276fa20e522bcf08eeb95b49143e9e4/books.json";
